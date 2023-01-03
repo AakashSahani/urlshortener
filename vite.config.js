d2de0218ca45b29c15537b43db8a9ba717e8f08e
Aakashsahani <sahani.aakash120@gmail.com>
2023-01-02 22:06:56 -0500

    Trying fix 3

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { icons } from 'react-icons';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), react - icons()],
-	build: {
-		rollupOptions: {},
-	},
+	plugins: [react()],
 });
