1f64fae02fcc575780b7d53eb59987184f5735ea
AakashSahani <sahaniaakash120@gmail.com>
2023-01-29 21:30:45 -0500

    Updated menu styling

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,15 +3,13 @@ import Working from '../../assets/illustration-working.svg';
 
 function Hero() {
 	return (
-		<section className="w-screen min-w-fit min-h-fit py-[10%] flex flex-col items-center">
-			{/* <div className="w-full h-fit"> */}
+		<section className="w-screen min-w-fit min-h-fit py-[10%] sm:py-10 flex flex-col sm:flex-row-reverse items-center sm:items-start">
 			<img
 				src={Working}
 				alt="Illustration of a working person"
 				className="translate-x-10"
 			/>
-			{/* </div> */}
-			<div className="flex flex-col justify-evenly text-center p-[5%] mb-20">
+			<div className="flex flex-col justify-evenly text-center sm:items-start sm:text-left p-[5%] mb-20">
 				<h1 className="text-4xl font-bold mt-10">
 					More than just shorter links
 				</h1>
@@ -19,7 +17,7 @@ function Hero() {
 					Build your brand's recognition and get detailed insights on how your
 					links are performing.
 				</p>
-				<button className="bg-mainCyan rounded-full min-w-fit w-1/2 h-12 text-white font-bold text-lg mx-auto">
+				<button className="bg-mainCyan rounded-full min-w-fit w-1/2 h-12 text-white font-bold text-lg mx-auto sm:mx-0">
 					Get Started
 				</button>
 			</div>
--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -9,30 +9,42 @@ function Header() {
 		setMenu((menu) => !menu);
 	};
 	return (
-		<header className="flex justify-between h-fit items-center px-5 py-10">
-			<div>
+		<header className="flex justify-between sm:justify-start h-fit items-center p-10 sm:px-[10%]">
+			<div className="">
 				<img src={logo} alt="Shortly" className="" />
 			</div>
-			<nav>
-				<ul className="hidden">
-					<li>
-						<a href="#">Features</a>
-					</li>
-					<li>
-						<a href="#">Pricing</a>
-					</li>
-					<li>
-						<a href="#">Resources</a>
-					</li>
-				</ul>
-				<div>
+			<nav className="sm:flex-1 sm:px-10">
+				<div className="hidden sm:flex justify-between items-center font-bold text-gray-400">
+					<ul className="flex items-center justify-evenly space-x-5">
+						<li>
+							<a href="#">Features</a>
+						</li>
+						<li>
+							<a href="#">Pricing</a>
+						</li>
+						<li>
+							<a href="#">Resources</a>
+						</li>
+					</ul>
+					<ul className="flex items-center justify-between space-x-5">
+						<li>
+							<a href="#">Login</a>
+						</li>
+						<li className="">
+							<button className="bg-mainCyan rounded-full w-full h-10 px-5 text-white text-lg">
+								Sign Up
+							</button>
+						</li>
+					</ul>
+				</div>
+				<div className="sm:hidden">
 					<button onClick={handleClick}>
 						<img src={hamburger} alt="Mobile menu" className="m-auto w-8" />
 					</button>
 					<ul
 						className={`${
 							menu ? 'block' : 'hidden'
-						} bg-darkViolet text-white z-10 absolute top-24 left-0 h-[50vh] w-11/12 mx-auto right-0 border-2 flex justify-center items-center`}
+						}  bg-darkViolet text-white font-bold z-10 absolute top-24 left-0 h-[45vh] w-10/12 mx-auto right-0 rounded-lg p-5 flex flex-col justify-evenly items-center text-center`}
 					>
 						<li>
 							<a href="#">Features</a>
@@ -42,6 +54,14 @@ function Header() {
 						</li>
 						<li>
 							<a href="#">Resources</a>
+						</li>
+						<li className="w-full border-t py-5">
+							<a href="#">Login</a>
+						</li>
+						<li className="w-full">
+							<button className="bg-mainCyan rounded-full w-full h-12 text-white font-bold text-lg mx-auto">
+								Sign Up
+							</button>
 						</li>
 					</ul>
 				</div>
